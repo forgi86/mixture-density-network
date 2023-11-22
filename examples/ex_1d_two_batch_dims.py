ca55cccb962dadf035ceee28ba2252552bd83de2
Marco Forgione <marco.forgione@supsi.ch>
2023-11-22 15:32:09 +0100

    - added support for multiple batch dimensions - added support for multiple samples

---
+++ examples/ex_1d_two_batch_dims.py
@@ -0,0 +1,77 @@
+from argparse import ArgumentParser
+import logging
+
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+import torch.optim as optim
+
+from src.blocks import MixtureDensityNetwork, NoiseType
+
+
+def gen_data(n):
+    y = np.linspace(-1, 1, n)
+    x = 7 * np.sin(5 * y) + 0.5 * y + 0.5 * np.random.randn(*y.shape)
+    return x[:,np.newaxis], y[:,np.newaxis]
+
+def plot_data(x, y):
+    plt.hist2d(x, y, bins=35)
+    plt.xlim(-8, 8)
+    plt.ylim(-1, 1)
+    plt.axis('off')
+
+
+if __name__ == "__main__":
+
+    n_components = 3
+    num_seq = 32
+    seq_len = 16
+    nx = 1
+    ny = 1
+
+
+    batch_size = num_seq * seq_len
+
+    plt.ion()
+    argparser = ArgumentParser()
+    argparser.add_argument("--n-iterations", type=int, default=2000)
+    args = argparser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    x, y = gen_data(n=batch_size)
+    x = torch.Tensor(x)
+    y = torch.Tensor(y)
+
+    x = x.reshape(num_seq, seq_len, nx)
+    y = y.reshape(num_seq, seq_len, nx)
+
+    model = MixtureDensityNetwork(nx, ny, n_components=n_components, hidden_dim=50, noise_type=NoiseType.DIAGONAL)
+    optimizer = optim.Adam(model.parameters(), lr=0.005)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.n_iterations)
+
+    for i in range(args.n_iterations):
+        optimizer.zero_grad()
+        loss = model.loss(x, y).mean()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        if i % 100 == 0:
+            logger.info(f"Iter: {i}\t" + f"Loss: {loss.data:.2f}")
+
+    with torch.no_grad():
+        y_samples = model.sample(x)
+
+    x = x.reshape(batch_size, nx)
+    y = y.reshape(batch_size, ny)
+    y_samples = y_samples.reshape(batch_size, ny)
+
+    plt.figure(figsize=(8, 3))
+    plt.subplot(1, 2, 1)
+    plot_data(x[:, 0].numpy(), y[:, 0].numpy())
+    plt.title("Observed data")
+    plt.subplot(1, 2, 2)
+    plot_data(x[:, 0].numpy(), y_samples[:, 0].numpy())
+    plt.title("Sampled data")
+    plt.show()
--- src/blocks.py
+++ src/blocks.py
@@ -24,7 +24,16 @@ class MixtureDensityNetwork(nn.Module):
     dim_out: int; dimensionality of the response variable
     n_components: int; number of components in the mixture model
     """
-    def __init__(self, dim_in, dim_out, n_components, hidden_dim, noise_type=NoiseType.DIAGONAL, fixed_noise_level=None):
+
+    def __init__(
+        self,
+        dim_in,
+        dim_out,
+        n_components,
+        hidden_dim,
+        noise_type=NoiseType.DIAGONAL,
+        fixed_noise_level=None,
+    ):
         super().__init__()
         assert (fixed_noise_level is not None) == (noise_type is NoiseType.FIXED)
         num_sigma_channels = {
@@ -47,7 +56,7 @@ class MixtureDensityNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, dim_out * n_components + num_sigma_channels)
+            nn.Linear(hidden_dim, dim_out * n_components + num_sigma_channels),
         )
 
     def forward(self, x, eps=1e-6):
@@ -60,8 +69,8 @@ class MixtureDensityNetwork(nn.Module):
         #
         log_pi = torch.log_softmax(self.pi_network(x), dim=-1)
         normal_params = self.normal_network(x)
-        mu = normal_params[..., :self.dim_out * self.n_components]
-        sigma = normal_params[..., self.dim_out * self.n_components:]
+        mu = normal_params[..., : self.dim_out * self.n_components]
+        sigma = normal_params[..., self.dim_out * self.n_components :]
         if self.noise_type is NoiseType.DIAGONAL:
             sigma = torch.exp(sigma + eps)
         if self.noise_type is NoiseType.ISOTROPIC:
@@ -70,25 +79,46 @@ class MixtureDensityNetwork(nn.Module):
             sigma = torch.exp(sigma + eps).repeat(1, self.n_components * self.dim_out)
         if self.noise_type is NoiseType.FIXED:
             sigma = torch.full_like(mu, fill_value=self.fixed_noise_level)
-        mu = mu.reshape(-1, self.n_components, self.dim_out)
-        sigma = sigma.reshape(-1, self.n_components, self.dim_out)
+        mu = mu.reshape(*mu.shape[:-1], self.n_components, self.dim_out)
+        sigma = sigma.reshape(*sigma.shape[:-1], self.n_components, self.dim_out)
         return log_pi, mu, sigma
 
     def loss(self, x, y):
         log_pi, mu, sigma = self.forward(x)
-        z_score = (y.unsqueeze(1) - mu) / sigma
+        z_score = (y.unsqueeze(-2) - mu) / sigma
         normal_loglik = (
-            -0.5 * torch.einsum("bij,bij->bi", z_score, z_score)
-            -torch.sum(torch.log(sigma), dim=-1)
+            -0.5 * torch.prod(z_score**2, dim=-1)  # correct for multi-dimensional?
+            # -0.5 * torch.einsum("bij,bij->bi", z_score, z_score)
+            - torch.sum(torch.log(sigma), dim=-1)
         )
         loglik = torch.logsumexp(log_pi + normal_loglik, dim=-1)
         return -loglik
 
-    def sample(self, x):
-        log_pi, mu, sigma = self.forward(x)
+    def sample(self, x, samples=1, squeeze=True):
+
+        batch_dims = x.dim() - 1 # number of initial dimensions to be treated as batch
+
+        log_pi, mu, sigma = self.forward(x) 
         cum_pi = torch.cumsum(torch.exp(log_pi), dim=-1)
-        rvs = torch.rand(len(x), 1).to(x)
+
+        # expand on sample dimension
+        cum_pi = cum_pi.unsqueeze(batch_dims).repeat(*[1]*batch_dims, samples, 1) # ..., S, C
+        mu = mu.unsqueeze(batch_dims).repeat(*[1]*batch_dims, samples, 1, 1) # ..., S, C, O
+        sigma = sigma.unsqueeze(batch_dims).repeat(*[1]*batch_dims, samples, 1, 1) # ..., S, C, O
+
+        # sample active mode
+        rvs = torch.rand(*x.shape[:-1], samples, 1)
         rand_pi = torch.searchsorted(cum_pi, rvs)
-        rand_normal = torch.randn_like(mu) * sigma + mu
-        samples = torch.take_along_dim(rand_normal, indices=rand_pi.unsqueeze(-1), dim=1).squeeze(dim=1)
-        return samples
+
+        # sample individual gaussians        
+        rand_normal = torch.randn_like(mu) * sigma + mu  #..., S, C, O 
+
+        # choose a random gaussian
+        out_samples = torch.take_along_dim(
+            rand_normal, indices=rand_pi.unsqueeze(-1), dim=batch_dims+1,
+        ).squeeze(batch_dims+1)
+
+        # squeeze if samples=1 and squeeze=1
+        if squeeze:
+            out_samples = out_samples.squeeze(batch_dims)
+        return out_samples
